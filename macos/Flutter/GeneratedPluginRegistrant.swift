80ac3d3f358a901965b24da133fc5e3642f7ac6d
Nasser Albusaidi <nasserbusaidi@gmail.com>
2026-06-07 18:12:54 +0400

    chore: drop unintended GeneratedPluginRegistrant regeneration (#248)
    flutter pub get regenerated the macOS plugin registrant in the worktree; not part of PR 2. Refs #248

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,7 +13,6 @@ import firebase_app_check
 import firebase_auth
 import firebase_core
 import firebase_messaging
-import flutter_local_notifications
 import package_info_plus
 import sentry_flutter
 import share_plus
@@ -29,7 +28,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
